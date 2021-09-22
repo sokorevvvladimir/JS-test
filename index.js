561cf4c35caeacb335b2ae9fd7d5662d4f726d23
sokorevvvladimir <sokorevvvladimir@gmail.com>
2021-09-22 21:49:34 +0300

    add lesson1

---
+++ index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// let a;
+
+// const b = 123;
+
+// a = 10;
+
+
+// console.log(a);
+// console.log(b);
+
+// const hasUser = "MAX";
+// console.log("hasUser:", hasUser);
+// const number = 4;
+// console.log("Number:", number);
+
+const getFirstNumber = prompt("Type a number!");
+console.log(getFirstNumber);
